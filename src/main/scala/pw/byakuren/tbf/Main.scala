2fae2c8b1158cfbd790af839c8ecc40073cb26e1
Brod8362 <brodriguez410@gmail.com>
2019-12-20 15:16:04 -0600

    yaml load

--- src/main/scala/pw/byakuren/tbf/Main.scala
+++ src/main/scala/pw/byakuren/tbf/Main.scala
@@ -1,8 +1,16 @@
 package pw.byakuren.tbf
 
-class Main {
+import java.io.{File, FileInputStream, InputStream}
+
+import org.yaml.snakeyaml.Yaml
+
+object Main {
 
   def main(args: Array[String]): Unit = {
+    val yaml = new Yaml()
+    val dir = System.getProperty("user.dir")
+    val input:InputStream = new FileInputStream(new File(dir+"/config.yaml"))
+    val output:java.util.LinkedHashMap[String,AnyRef] = yaml.load(input)
 
   }
 
